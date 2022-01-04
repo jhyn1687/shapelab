228414ec438aab0843e83eb313b6212403c19b78
Tony Yuan <jhyuan01@gmail.com>
2022-01-04 15:12:54 -0800

    material UI + cname

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
+      <Button>Hello World!</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
