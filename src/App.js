8bf1525597f9e90bda0f4a680eaa9a412eefb4de
Tony Yuan <jhyuan01@gmail.com>
2022-01-04 19:35:46 -0800

    added navbar, initized pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+import Projects from'./pages/Projects';
+import Contact from'./pages/Contact';
+import Footer from './components/Footer/Footer';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Button>Hello World!</Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import { Button } from '@mui/material';
+import React from 'react';
+import {NavLink, Nav, Bars, NavMenu} from './NavbarComps';
+// import Logo from'../../images/logo.svg';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Nav>
+        <NavLink to="/">
+          <h1 style={{
+            textDecoration:'none',
+          }}>Tony Yuan</h1>
+          {/* <img src={Logo} alt='logo' /> */}
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/">
+            <h1>Home</h1>
+          </NavLink>
+          <NavLink to="/Projects">
+           <h1>Projects</h1>
+          </NavLink>
+          <NavLink to="/contact">
+            <h1>Contact</h1>
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </div>
+  )
+};
+
+export default Navbar;
