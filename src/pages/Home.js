5f883bd4dd93a6e3932c86435b5017d0810a539c
Tony Yuan <jhyuan01@gmail.com>
2022-01-20 13:45:21 -0800

    ported from personal website

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <>
       <Helmet>
-        <title>Tony Yuan | Home</title>
+        <title>Shapelab | Home</title>
       </Helmet>
-      <div className='container'>
-        <h1 className='content'>Home</h1>
-        <p className='content'>Hi, my name is Tony. This website is currently work in progress. Come back later to check it out!</p>
+      <div className="container">
+        <h1 className="content">Home</h1>
+        <p className="content">
+          Our lab investigates the neural basis of visual shape representation
+          and recognition. Our focus is area V4, an intermediate stage in the
+          shape processing pathway in primates.
+        </p>
+        <p className="content">
+          We use a combination of single-cell neurophysiology experiments,
+          behavioral manipulations and computational modeling to address
+          questions about how visual stimuli are represented in the brain, how
+          these representations arise and how they ultimately underlie behavior.
+        </p>
+        <p className="content">
+          We are a part of the Department of Biological Structure at the
+          University of Washington in Seattle, WA. For further details about our
+          research, see Publications or contact Anitha Pasupathy.
+        </p>
+        <p className="content">
+          If you are interested in joining our team, check the Positions and
+          Contact tab.
+        </p>
       </div>
     </>
-  )
+  );
 };
 
 export default Home;
---
+++ src/pages/Publications.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Publications | Home</title>
+      </Helmet>
+      <div className="container">
+        <h1 className="content">Publications</h1>
+        <p className="content"></p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
